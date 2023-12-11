a3657e920f51c310dee9c8ba3709d43db8e89d22
bfsadmin <bfsadmin@MacBook-Air.local>
2023-12-11 09:46:54 -0500

    initial commit

---
+++ Themes/Unity/bd-scss.config.js
@@ -0,0 +1,12 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'Unity',
+		author: 'super.user',
+        authorId: '419610859392860162',
+		version: '1.2',
+		description: 'A theme inspired by Samsung\'s OneUI',
+		source: 'https://github.com/joshuah345/Discord-Stuff/tree/master/Themes/Unity',
+        invite: 'BDGSZDYSQ',
+	},
+};
