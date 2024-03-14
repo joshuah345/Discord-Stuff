045f8781bbcc0105aadf851626387870a7dca2c4
joshuah345 <joshuah345@gmail.com>
2024-03-14 09:00:32 -0400

    testing stuffs

---
+++ Themes/Orion/README.md
@@ -0,0 +1,3 @@
+# Orion
+
+an experimental theme.
---
+++ Themes/Orion/bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: "Orion",
+		author: "joshuah345",
+		version: "0.0.7",
+		description: "an experimental theme.",
+		source: "https://github.com/joshuah345/Discord-Stuff/Themes/Orion"
+	}
+}
